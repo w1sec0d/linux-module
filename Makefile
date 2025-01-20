7082c8158a10d459531303a0138ee6a11c9ae109
Carlos Ramírez <cramirezmun@unal.edu.co>
2025-01-20 10:16:06 -0500

    Add golden ratio and gcd printing

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
 obj-m += simple.o
 
 all:
-    # Ensure this line starts with a tab
     make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules CC=x86_64-linux-gnu-gcc-13
 
 clean:
-    # Ensure this line starts with a tab
     make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
--- simple.c
+++ simple.c
@@ -1,18 +1,23 @@
 #include <linux/init.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/hash.h>
+#include <linux/gcd.h>
 
 /* This function is called when the module is loaded. */
 static int __init simple_init(void)
 {
     printk(KERN_INFO "Loading Kernel Module\n");
+    printk(KERN_INFO "%llu∖n", GOLDEN_RATIO_PRIME);
     return 0;
 }
 
 /* This function is called when the module is removed. */
 static void __exit simple_exit(void)
 {
+    unsigned long result = gcd(3300, 24);
     printk(KERN_INFO "Removing Kernel Module\n");
+    printk(KERN_INFO "%lu\n", result);
 }
 
 /* Macros for registering module entry and exit points. */
